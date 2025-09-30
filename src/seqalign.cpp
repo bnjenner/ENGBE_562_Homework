e945ba571523add9847cb940f0a5275d70370334
bnjenner <bnjenner@bu.edu>
2025-09-29 21:18:44 -0400

    Final Version

--- src/seqalign.cpp
+++ src/seqalign.cpp
@@ -13,7 +13,7 @@ Function Definitions found towards the bottom of the file. :)
 */
 
 ////////////////////////////////////////////////////////////////////
-// Object in Dynamic Programming Matrix
+// Object in Dynamic Programming F Matrix
 struct State {
     
 public:
@@ -29,6 +29,7 @@ public:
     State(int _a) : M(_a), D(_a), I(_a), max(_a), visited(true) {}
     State(int _m, int _d, int _i) : M(_m), D(_d), I(_i), visited(false) {}
 
+    // Make State Decision
     void set_max() {
         max = std::max(std::max(M, D), I);
         visited = true;
@@ -58,58 +59,86 @@ class Alignment {
 
 private:
 
-    int n, m;
-    int score = 0;
+    int n, m;                                  // Length of sequences
 
-    int match = 1;
-    int mismatch = -1;
-    int gap = -2;
+    int match = 1;                             // Match Reward
+    int mismatch = -1;                         // Mismatch Penality
+    int gap = -2;                              // Gap Penality
 
-    std::string seq1, seq2;
-    std::vector<std::string> tb;
-    std::vector<std::vector<State>> dp;
+    int max_score = -1;                        // Max Alignment Score
+    int max_i = -1, max_j = -1;                // Max Position
+    int tb_i = -1, tb_j = -1;                  // Traceback Start Position
 
-    void align_itr();
-    void align_rec(const int i, const int j);
-    void traceback(const int i, const int j, const int x, std::vector<std::string> &alns);
+    bool flip = false;                         // Flip Sequences in Output (not necessary)
+
+    std::string seq1, seq2;                    // Input Sequences
+    std::vector<std::string> tb;               // Traceback Paths
+    std::vector<std::vector<State>> dp;        // F Matrix
+
+    //////////////////////////////////
+    // Private Alignment Methods
+    void align_itr(const std::string &method);
+    void align_rec(const int i, const int j, const std::string &method);
+    void traceback(const int i, const int j, const int x, std::vector<std::string> &alns, const std::string &method);
     
+    // Private Scoring Utilities
+    int get_max(const std::vector<int> vec, const std::string &method);
+    int score_funct(const int &i, const int &j, const char c, const std::string &method);
+    void record_max(const int &i, const int &j, const std::string &method) {
+        // Keep track of max scores for sw and sg
+        if (method == "sw" && max_score < dp[i][j].max) {
+            max_score = dp[i][j].max;
+            max_i = i, max_j = j;   
+        } else if (method == "sg" && j == this -> m && max_score < dp[i][j].max) {
+            max_score = dp[i][j].max;
+            max_i = i;
+        }
+    }
+
 public:
 
+    //////////////////////////////////
+    // Constructors 
     Alignment() {}
-
     Alignment(const std::string &s1, const std::string &s2) : seq1(s1), seq2(s2) {
-        n = seq1.size();
-        m = seq2.size();
+        n = seq1.size(); m = seq2.size();
+        if (m > n) { // Flip to keep seq2 smallest
+            n = seq2.size(); m = seq1.size();
+            seq1 = s2; seq2 = s1;
+            flip = true;
+        }
         dp = std::vector<std::vector<State>>(n + 1, std::vector<State>(m + 1));
     }
 
+    //////////////////////////////////
+    // Public Methods
+    int align(const std::string &method, const std::string &algo);
+    int get_score() const { return max_score; }
+    void print_alignment() const;
     void reset() {
-        score = 0;
+        max_score = -1;
+        max_i = -1, max_j = -1;
+        tb_i = -1, tb_j = -1;
         tb.clear();
         dp = std::vector<std::vector<State>>(n + 1, std::vector<State>(m + 1));
     }
-
-    int get_score() const { return score; }
-    int get_max(const int &a, const int &b, const int &c) {
-        return std::max(std::max(a, b), c);
-    }
-
-    int align(const std::string &method, const std::string &algo);
-    void print_alignment() const;
-    void print_dp_matrix() const;
 };
 
 
 /////////////////////////////////////////////////////////////
-// Utility Functions
+// Utility Functions (Defined Below)
 void print_help();
 std::vector<std::string> read_fasta(const std::string &file);
+
 
 /////////////////////////////////////////////////////////////
 // Main
 int main(int argc, char **argv) {
 
-    std::string type, mode;
+    // Option Variables
+    int _t, _m, opt;
+    int option_index = 0;
+    std::string type = "nw", mode = "itr";
 
     // Define long options
     static struct option long_options[] = {
@@ -119,18 +148,12 @@ int main(int argc, char **argv) {
         {nullptr, 0, nullptr, 0}
     };
 
-    int _t, _m;
-    int opt;
-    int option_index = 0;
-
     // Parse optional arguments
-    while ((opt = getopt_long(argc, argv, "t:m:", long_options, &option_index)) != -1) {
-        
+    while ((opt = getopt_long(argc, argv, "t:m:", long_options, &option_index)) != -1) {   
         switch (opt) {
             case 'h':
                 print_help();
-                exit(1);
-
+                return 1;
             case 't':
                 _t = std::atoi(optarg);
                 if (_t < 0 || _t > 3) {
@@ -139,13 +162,12 @@ int main(int argc, char **argv) {
                     return 1;
                 } else if (_t == 0) {
                     type = "nw";
-                } else if (_t == 1) {
+                } else if (_t == 1 || _t == 2) {
                     type = "sg";
                 } else {
                     type = "sw";
                 }
                 break;
-
             case 'm':
                 _m = std::atoi(optarg);
                 if (_m < 0 || _m > 1) {
@@ -158,7 +180,6 @@ int main(int argc, char **argv) {
                     mode = "rec";
                 }
                 break;
-
             default:
                 print_help();
                 return 1;
@@ -172,10 +193,14 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    // Read in Fasta Files
     std::vector<std::string> seq1 = read_fasta(argv[optind]);
     std::vector<std::string> seq2 = read_fasta(argv[optind + 1]);
 
+    // Create Alignment Object
     Alignment aln(seq1[0], seq2[0]);
+
+    // Align and Print Alignments
     int score = aln.align(type, mode); 
     aln.print_alignment();
 
@@ -197,12 +222,12 @@ void print_help() {
 }
 
 
+////////////////////////////////////////////////////////////////////
 // Process FASTA file into vector of sequences
 std::vector<std::string> read_fasta(const std::string &file) {
 
     std::string seq, id, line;
     std::vector<std::string> sequences;
-
     std::ifstream fa(file);
     if (fa.is_open()) {
    
@@ -230,33 +255,66 @@ std::vector<std::string> read_fasta(const std::string &file) {
 }
 
 
+/////////////////////////////////////////////////////////////
+// Get Max of Vector Utility
+int Alignment::get_max(const std::vector<int> vec, const std::string &method) {
+    int res = vec[0];
+    if (method == "sw") { res = 0; } // Maximum Score in SW is 0.
+    for (const auto &v: vec) { res = std::max(res, v); }
+    return res;
+}
+
+
+/////////////////////////////////////////////////////////////
+// F Matrix Scoring Function
+int Alignment::score_funct(const int &i, const int &j, const char c, const std::string &method) {
+    int score, s;
+    if (c == 'M') {
+        s = match;
+        if (seq1[i - 1] != seq2[j - 1]) { s = mismatch; }   
+        score = Alignment::get_max({
+                                    dp[i-1][j-1].M + s,
+                                    dp[i-1][j-1].D + s,
+                                    dp[i-1][j-1].I + s
+                                    },
+                                   method);
+    } else if (c == 'D') {
+        score = Alignment::get_max({dp[i][j-1].max + gap}, method);
+    } else {
+        score = Alignment::get_max({dp[i-1][j].max + gap}, method);
+    }
+    return score;
+}
+
+
 
 /////////////////////////////////////////////////////////////
 // Perform Traceback
-void Alignment::traceback(const int i, const int j, const int x, std::vector<std::string> &alns) {
+void Alignment::traceback(const int i, const int j, const int x, 
+                          std::vector<std::string> &alns, const std::string &method) {
 
     // Initilize Alignments Vector
     if (alns.empty()) { alns.push_back(""); }
 
-    // End of Alignment Cases
+    // Termination / Special Traceback Cases
     if (i <= 0 && j <= 0) {
-        return;
+        return; 
     } else if (i == 0 && j > 0) {
         alns[x] += "D";
-        traceback(i, j - 1, x, alns);
+        traceback(i, j - 1, x, alns, method);
         return;
-    } else if (i > 0 && j == 0) {
+    } else if (i > 0 && j == 0 || (max_i < i && method == "sg")) {
+        if (method == "sw") { return; }
         alns[x] += "I";
-        traceback(i - 1, j, x, alns);
+        traceback(i - 1, j, x, alns, method);
         return;
-    } 
-
+    }
 
     int t;
     std::string t_str = alns[x]; 
     std::vector<char> states = dp[i][j].get_trace();
 
-    // Recurse for all maximum states
+    // Recurse for all maximum states (possible paths)
     for (int y = 0; y < states.size(); y++) {
 
         t = x;  
@@ -264,18 +322,18 @@ void Alignment::traceback(const int i, const int j, const int x, std::vector<std
         // If multiple paths
         if (y > 0) {
             alns.push_back(t_str);
-            t = alns.size() - 1;
+            t = alns.size() - 1; // New index
         }
         
         if (states[y] == 'M') {
             alns[t] += "M";
-            traceback(i - 1, j - 1, t, alns);  
+            traceback(i - 1, j - 1, t, alns, method);
         } else if (states[y] == 'I') {
             alns[t] += "I";
-            traceback(i - 1, j, t, alns);
+            traceback(i - 1, j, t, alns, method);
         } else if (states[y] == 'D') {
             alns[t] += "D";
-            traceback(i, j - 1, t, alns);
+            traceback(i, j - 1, t, alns, method);
         }
 
     }   
@@ -283,52 +341,39 @@ void Alignment::traceback(const int i, const int j, const int x, std::vector<std
 
 
 /////////////////////////////////////////////////////////////
-// Generic Dynamic Programming Implementation
-void Alignment::align_itr() {
+// Iterative Dynamic Programming Implementation
+void Alignment::align_itr(const std::string &method) {
 
-    int s;
-    for (int i = 1; i <= seq1.size(); i++) {    
-        for (int j = 1; j <= seq2.size(); j++) {
+    for (int i = 1; i <= n; i++) {    
+        for (int j = 1; j <= m; j++) {
 
-            s = match;
-            if (seq1[i - 1] != seq2[j - 1]) {
-                s = mismatch;
-            }
-
-            dp[i][j].M = Alignment::get_max(
-                            dp[i-1][j-1].M + s,
-                            dp[i-1][j-1].D + s,
-                            dp[i-1][j-1].I + s
-                          );
-            dp[i][j].D = dp[i][j-1].max + gap;
-            dp[i][j].I = dp[i-1][j].max + gap;
+            dp[i][j].M = Alignment::score_funct(i, j, 'M', method);
+            dp[i][j].D = Alignment::score_funct(i, j, 'D', method);
+            dp[i][j].I = Alignment::score_funct(i, j, 'I', method);    
             dp[i][j].set_max();
+            Alignment::record_max(i, j, method);
         }
     }
 }
 
-void Alignment::align_rec(const int i, const int j) {
+
+/////////////////////////////////////////////////////////////
+// Recursive Dynamic Programming Implementation
+void Alignment::align_rec(const int i, const int j, const std::string &method) {
 
     int s;
     if (dp[i][j].visited == true) { return; }
 
-    align_rec(i-1, j-1);
-    align_rec(i, j-1);
-    align_rec(i-1, j);
+    align_rec(i-1, j-1, method);
+    align_rec(i, j-1, method);
+    align_rec(i-1, j, method);
 
-    s = match;
-    if (seq1[i - 1] != seq2[j - 1]) {
-        s = mismatch;
-    }
+    dp[i][j].M = Alignment::score_funct(i, j, 'M', method);
+    dp[i][j].D = Alignment::score_funct(i, j, 'D', method);
+    dp[i][j].I = Alignment::score_funct(i, j, 'I', method);
 
-    dp[i][j].M = Alignment::get_max(
-                    dp[i-1][j-1].M + s,
-                    dp[i-1][j-1].D + s,
-                    dp[i-1][j-1].I + s
-                  );
-    dp[i][j].D = dp[i][j-1].max + gap;
-    dp[i][j].I = dp[i-1][j].max + gap;
     dp[i][j].set_max();
+    Alignment::record_max(i, j, method);
 }
 
 
@@ -336,23 +381,22 @@ void Alignment::align_rec(const int i, const int j) {
 // General Alignment Call
 int Alignment::align(const std::string &method, const std::string &algo) {
         
+    tb_i = n;
+    tb_j = m;
+
     // Initialize Scoring Penalities and Boundary Conditions
     if (method == "nw") {
         for (int i = 0; i <= n; i++) { dp[i][0] = State(gap * i); }
         for (int j = 0; j <= m; j++) { dp[0][j] = State(gap * j); }
-
+        max_i = this -> n;
+        max_j = this -> m;
     } else if (method == "sg") {
-        // TO DO
-        if (seq1.size() > seq2.size()) {
-            for (int j = 0; j <= seq2.size(); j++) { dp[0][j] = State(gap * j); }
-        } else {
-            for (int i = 0; i <= seq1.size(); i++) { dp[i][0] = State(gap * i); } 
-        }
-
+        for (int i = 0; i <= n; i++) { dp[i][0].set_max(); }
+        for (int j = 0; j <= m; j++) { dp[0][j] = State(gap * j); }
+        max_j = this -> m;
     } else if (method == "sw") {
-        // TO DO
-        ;
-
+        for (int i = 0; i <= n; i++) { dp[i][0].set_max(); }
+        for (int j = 0; j <= m; j++) { dp[0][j].set_max(); }
     } else {
         std::cerr << "Error: Unknown alignment method " << method << "\n";
         exit(1);
@@ -360,46 +404,21 @@ int Alignment::align(const std::string &method, const std::string &algo) {
 
     // Perform Alignment
     if (algo == "rec") {
-        align_rec(n, m);
+        align_rec(n, m, method);
     } else {
-        align_itr();
+        align_itr(method);
     }
 
-    score = dp[n][m].max;
+    // Adjust Traceback Starts
+    if (method == "sw") { tb_i = max_i; tb_j = max_j; }
+    if (method == "sg") { tb_i = n; }
 
     // Traceback Alignments
     int num_alignments = 0;
-    traceback(n, m, num_alignments, this -> tb);
+    traceback(tb_i, tb_j, num_alignments, this -> tb, method);
 
-    return score;
+    return Alignment::get_score();
 }
-
-
-/////////////////////////////////////////////////////////////
-// Print Dynamic Programming Matrix
-void Alignment::print_dp_matrix() const {
-    std::cerr << "X\tX\t";
-    for (int x = 0; x < seq2.size(); x++) { std::cerr << seq2[x] << "\t"; }
-    std::cerr << "\n";
-    for (int i = 0; i <= seq1.size(); i++) {
-        if (i == 0) {
-            std::cerr << "X\t";
-        } else {
-            std::cerr << seq1[i - 1] << "\t";
-        }
-        for (int j = 0; j <= seq2.size(); j++) {
-            if (i == 0) {
-                std::cerr << dp[i][j].D << "\t";
-            } else if (j == 0) {
-                std::cerr << dp[i][j].I << "\t";
-            } else {
-                std::cerr << dp[i][j] << "\t";
-            }
-        }
-        std::cerr << "\n";
-    }
-}
-
 
 /////////////////////////////////////////////////////////////
 // Print Alignment
@@ -408,32 +427,36 @@ void Alignment::print_alignment() const {
     int i, j, x = 0;
     std::string aln_seq1, aln_seq2;
 
+    // For each possible alignment
     for (auto &t : tb) {
         
-        i = 0; j = 0;
+        i = tb_i; j = tb_j;
         aln_seq1 = ""; aln_seq2 = "";
 
-        std::string str = t;
-        std::reverse(str.begin(), str.end());
-
-        for (const auto &c : str) {
+        // Iterate through traceback strings
+        for (const auto &c : t) {
             if (c == 'M') {
-                aln_seq1 += seq1[i];
-                aln_seq2 += seq2[j];
-                i += 1; j += 1;
+                aln_seq1 = seq1[i - 1] + aln_seq1;
+                aln_seq2 = seq2[j - 1] + aln_seq2;
+                i -= 1; j -= 1;
             } else if (c == 'I') {
-                aln_seq1 += seq1[i];
-                aln_seq2 += "_";
-                i += 1;
+                aln_seq1 = seq1[i - 1] + aln_seq1;
+                aln_seq2 = "_" + aln_seq2;
+                i -= 1;
             } else if (c == 'D') {
-                aln_seq1 += "_";
-                aln_seq2 += seq2[j];
-                j += 1;
+                aln_seq1 = "_" + aln_seq1;
+                aln_seq2 = seq2[j - 1] + aln_seq2;
+                j -= 1;
             }
         }
 
-        std::cout << "Alignment #" << x + 1 << " (Score: " << this -> score << "):\n";
-        std::cout << aln_seq1 << "\n" << aln_seq2 << "\n\n";
+        // Report Score and Alignments
+        std::cout << "Alignment #" << x + 1 << " (Score: " << this -> max_score << "):\n";
+        if (flip) {
+            std::cout << aln_seq2 << "\n" << aln_seq1 << "\n\n";
+        } else {
+            std::cout << aln_seq1 << "\n" << aln_seq2 << "\n\n";
+        }
         x += 1;
     }
 }
